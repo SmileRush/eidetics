5eb5c11d3cfc336ae6a7237d8cc6bd4b135a979d
SmileRush <91185902+SmileRush@users.noreply.github.com>
2023-10-04 17:19:28 +0900

    v 1.1.20

--- src/Types/Components/ClassAttributes.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export interface ClassAttributes extends React.ClassAttributes<HTMLElement> {
-    ref?: React.LegacyRef<HTMLElement> | undefined;
-}
--- src/Types/Components/index.d.ts
+++ src/Types/Components/index.d.ts
@@ -1,6 +1,5 @@
-import { ClassAttributes } from './ClassAttributes';
 import { HTMLAttributes } from './HTMLAttributes';
 import { DOMAttributes } from './DOMAttributes';
 interface AllComponentsProps extends DOMAttributes, HTMLAttributes {
 }
-export { ClassAttributes, DOMAttributes, HTMLAttributes, AllComponentsProps, };
+export { DOMAttributes, HTMLAttributes, AllComponentsProps, };
