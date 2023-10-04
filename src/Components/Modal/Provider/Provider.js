568b38634ca1c9128ba8d861d988512e3685c7e7
SmileRush <91185902+SmileRush@users.noreply.github.com>
2023-10-04 17:38:27 +0900

    v 1.1.21

--- src/Components/Modal/Provider/Provider.js
+++ src/Components/Modal/Provider/Provider.js
@@ -1,2 +1,2 @@
-import{createPortal as r}from"react-dom";import{Context as o}from"./Context.js";import{_useModal as t,_useModalPortal as e}from"../Hooks/useModal.js";import{jsx as m}from"react/jsx-runtime";var i=function(i){var n=i.children,a=t(),l=e();return l?r(m(o.Provider,{value:a,children:n}),l):null};export{i as Provider};
+import{Context as r}from"./Context.js";import{_useModal as o}from"../Hooks/useModal.js";import{jsx as e}from"react/jsx-runtime";var t=function(t){var i=t.children,m=o();return e(r.Provider,{value:m,children:i})};export{t as Provider};
 //# sourceMappingURL=Provider.js.map
